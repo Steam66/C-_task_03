54cd52eb05ee73156e5a0b7e769544d281990d6d
Кирилл Парыгин <parygin.ks@gmail.com>
2023-09-23 16:37:13 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Задача_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите первое число: ");
+            string a1= Console.ReadLine();
+            Console.Write("Введите второе число: ");
+            string a2 = Console.ReadLine();
+            Console.Write("Введите третье число: ");
+            string a3 = Console.ReadLine(); 
+            Console.Write("Введите четвертое число: ");
+            string a4 = Console.ReadLine(); 
+            Console.WriteLine("Результат: "+a1+a2+a3+a4);
+        }
+    }
+}
